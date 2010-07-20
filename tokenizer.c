debdfe2238f0e5d872877e51bd8df8746825d6e8
Yeison Rodriguez <yeirodr@gmail.com>
2010-07-19 21:28:44 -0400

    Cleaning up the code some more.

--- tokenizer.c
+++ tokenizer.c
@@ -22,7 +22,11 @@ char getNextToken(char *buffer, FILE *file){
 		exit(1);
 	}
 	
-	/* regexec() returns 0 if the string passed to it contains the sequence specified by the regular expression.  In this case the regular expression is any kind of white space, and the we are passing one character at a time to the regexec function.  Therefore, if the character passed is a white space, the if statement fails and the white space is ignored. */	
+	/* regexec() returns 0 if the string passed to it contains the sequence 
+	 specified by the regular expression.  In this case the regular expression 
+	 is any kind of white space, and the we are passing one character at a time 
+	 to the regexec function.  Therefore, if the character passed is a white 
+	 space, the if statement fails and the white space is ignored. */	
 	while ((c = getc(file)) != EOF){ 
 		//While the next character is not the end of the file.
 		
@@ -32,20 +36,25 @@ char getNextToken(char *buffer, FILE *file){
 		newStatus = regexec(&regularExpression, &c, 0, NULL, REG_NOTBOL);
 		newStatus = regexec(&regularExpression, &c, 0, NULL, REG_NOTBOL);
 		//Get the new status; is the current character a white space?
-		/* The reason I'm calling the regexec twice, is because there seems to be a bug. When I was testing my code with the Temperature module, the 9th call kept returning a zero, despite the fact that the 9th character is an 'r'.  Calling the function twice seems to circumvent the bug.*/
+		/* The reason I'm calling the regexec twice, is because there seems to 
+		 be a bug. When I was testing my code with the Temperature module, the 
+		 9th call kept returning a zero, despite the fact that the 9th character 
+		 is an 'r'.  Calling the function twice seems to circumvent the bug.*/
 		
-		/* If the current character and the previous character are both white spaces,
-		 do nothing.  Continue to read the next character.*/
+		/* If the current character and the previous character are both white 
+		 spaces,do nothing.  Continue to read the next character.*/
 		if((oldStatus == 0) && (newStatus == 0)){
 			continue;
 		}
-		/* If the new character and the old characters are both none white space, append the new character to the buffer.*/
+		/* If the new character and the old characters are both none white 
+		 space, append the new character to the buffer.*/
 		else if(newStatus) {
 			buffer[tokenLength] = c;
 			//strcat(buffer, &c);
 			tokenLength++;
 		}
-		/*  Otherwise, if the current character is a white space but the previous one was not, finish */
+		/*  Otherwise, if the current character is a white space but the 
+		 previous one was not, finish */
 		else {
 			regfree(&regularExpression);
 			buffer[tokenLength] = '\0';
